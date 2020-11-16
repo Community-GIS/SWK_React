ab77d13847b05e554e87fa9eb1f7f423c8d23549
AnimeshN <animesh.nauti@gmail.com>
2020-11-16 11:46:50 +0530

    barchart options added

--- src/Barchart/Barchart.js
+++ src/Barchart/Barchart.js
@@ -6,21 +6,52 @@ import { calMonthlyData } from '../util';
 function Barchart({data,selLane,selCategory}) {
     // const [state,setState] = useState();
     const barData = calMonthlyData(data,selLane,selCategory);
+    let titleText = `${selCategory} Waste Monthly Distribution`;
+    titleText = titleText.charAt(0).toUpperCase() + titleText.slice(1)
 
     return (
         <div className="barchart">
         <Bar
           data={barData.chartData}
           options={{
-            // title:{
-            //   display:this.props.displayTitle,
-            //   text:'Largest Cities In '+this.props.location,
-            //   fontSize:25
-            // },
-            // legend:{
-            //   display:this.props.displayLegend,
-            //   position:this.props.legendPosition
-            // }
+            title:{
+              display:true,
+              text:titleText,
+              fontSize:15
+            },
+            legend:{
+              display:false
+              // position:
+            },
+            scales:{
+              yAxes:[
+                {
+                  ticks:{
+                    beginAtZero: true,
+                    // stepSize:200
+                  },
+                  
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'total waste(Kg)'
+                    },
+
+                    gridLines: {
+                      // display:false,
+                      // drawOnChartArea: false
+                  }
+                  
+                }
+              ],
+              xAxes:[{
+                gridLines:{
+                  // display:false
+                  drawOnChartArea: false
+                }
+              }
+              ]
+            }
+
           }}
         />
         </div>
